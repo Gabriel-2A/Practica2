8c70478be4b6bc9b60478178ff32a3289daeb251
Gabriel-2A <gaa.021205@gmail.com>
2022-10-07 09:46:51 -0500

    Modificaciones Para otra practica

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -19,8 +19,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private EditText etGuardar;
-    private Button bAgregar;
-    private TextView tvSueldo;
+    private Button bAgregar, bBuscar;
+    private TextView tvSueldo, tvPosicion, tvImparOPar;
     private Nodo primero = null;
     private ListView lvLista;
     private ArrayList <Double> list;
@@ -32,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         etGuardar = findViewById(R.id.etGuardar);
         bAgregar = findViewById(R.id.bAgregar);
+        bBuscar = findViewById(R.id.bBuscar);
         tvSueldo = findViewById(R.id.tvSueldo);
+        tvPosicion = findViewById(R.id.tvPosicion);
+        tvImparOPar = findViewById(R.id.tvImparOPar);
         lvLista = findViewById(R.id.lvLista);
 
         list = new ArrayList<>();
@@ -73,6 +76,46 @@ public class MainActivity extends AppCompatActivity {
 
                 etGuardar.setText(null);
 
+            }
+        });
+
+        bBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (etGuardar.getText().toString().trim().equals("") || etGuardar.getText().toString().trim().equals(".")) {
+                    Toast.makeText(getApplicationContext(), "Dato no aceptado", Toast.LENGTH_SHORT).show();
+                } else {
+                    double aux = Double.parseDouble(etGuardar.getText().toString().trim());
+                    int cont = 0, indice = -1;
+                    Nodo auxPri = primero;
+                    while (auxPri != null) {
+                        cont++;
+                        if (aux == auxPri.getDato()) {
+                            indice = cont;
+                            break;
+                        }
+                        auxPri = auxPri.getEnlace();
+                    }
+                    if (indice == -1) {
+                        tvPosicion.setText("No existe el dato");
+                        System.out.println("No existe el dato");
+                    } else {
+                        Log.i("Log", "El sueldo se encuentra en la posicion: " + cont);
+                        tvPosicion.setText("El sueldo se encuentra en la posicion: " + cont);
+
+                        if (indice % 2 == 0) {
+                            Log.i("Log", "La posicion del sueldo es par");
+                            tvImparOPar.setText("La posicion del sueldo es par");
+                        } else {
+                            Log.i("Log", "La posicion del sueldo es impar");
+                            tvImparOPar.setText("La posicion del sueldo es impar");
+                        }
+
+                    }
+
+
+                }
             }
         });
 
